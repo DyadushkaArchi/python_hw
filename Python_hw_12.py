ba1cd06442c2b83d02b0e8cc1412e7330c90aab6
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-11-16 16:08:11 +0200

    Подправил домашнее задание №12 по функциям

--- Python_hw_12.py
+++ Python_hw_12.py
@@ -7,20 +7,20 @@ import math
 #введенного пользователем в консоли, без использования операторов цикла.
 #Реализовать задачу без использования строк.
 
-def amount(xyz):
-    action1 = xyz / 100
+def amount(number):
+    action1 = number / 100
     result1 = int(action1)
 
-    action2 = math.fmod(243, 100)
+    action2 = math.fmod(number, 100)
     action2_1 = action2 / 10
     result2 = int(action2_1)
 
-    action3 = math.fmod(xyz, 10)
+    action3 = math.fmod(number, 10)
     result3 = action3
 
     total_result = result1 + result2 + result3
     return total_result
 
-example = amount(243)
+example = amount(244)
 print(int(example))
 
