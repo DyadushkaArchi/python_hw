fd5a2e473a8c51b1b7fd0811c43cf8b1ee1b8d37
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-11-25 10:42:46 +0200

    По всей видимости исправил Задание №17)

--- Python_hw_17.py
+++ Python_hw_17.py
@@ -9,17 +9,16 @@ def solve_quadratic_equation(a, b, c): # return 2 values
     discriminant = b**2 - 4*a*c
 
     if discriminant > 0:
-        x1 = (-b + math.sqrt(discriminant)) / 2*a
-        x2 = (-b - math.sqrt(discriminant)) / 2*a
+        x1 = (-b + math.sqrt(discriminant)) / (2*a)
+        x2 = (-b - math.sqrt(discriminant)) / (2*a)
         return x1, x2
     if discriminant == 0:
-        x1 = (-b + math.sqrt(discriminant)) / 2*a
+        x1 = (-b + math.sqrt(discriminant)) / (2*a)
         return x1, None
     else:
         return None, None
 
 
-example = solve_quadratic_equation(1, 4, 20)
+example = solve_quadratic_equation(1, 4, -9)
 print(example)
-
 
