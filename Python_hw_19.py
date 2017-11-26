c2e16ca2290480043357ab0479a72d83ee63facc
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-11-26 13:25:52 +0200

    Подправил домашнее задание №19 по циклам

--- Python_hw_19.py
+++ Python_hw_19.py
@@ -2,6 +2,7 @@ import random
 
 def find_min_max_diff(num_limit, lower_bound, upper_bound):
 
+    current_min = upper_bound
     current_max = lower_bound
     for i in range(num_limit):
 
@@ -11,19 +12,14 @@ def find_min_max_diff(num_limit, lower_bound, upper_bound):
         if rand_number > current_max:
             current_max = rand_number
 
-
-    current_min = upper_bound
-    for i in range(num_limit):
-
-        rand_number = random.randint(lower_bound, upper_bound)
-        print('rand.number: ', rand_number)
-
         if rand_number < current_min:
             current_min = rand_number
 
-
     result = current_max - current_min
     return result
+
+
+
 
 result = find_min_max_diff(10, 100, 200)
 print("Diff between max and min: %d" % result)
