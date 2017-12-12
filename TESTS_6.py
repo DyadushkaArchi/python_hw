8e1cfb179303103222e2feb55770466bbc59752d
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-12-12 21:26:20 +0200

    TESTS_6 corrected

--- TESTS_6.py
+++ TESTS_6.py
@@ -5,17 +5,18 @@
 
 
 def is_isogramm(text):
-    lst = {}
-    for i in range(len(str(text))):
-        key = str(text)[i]
-        if not str(key) in lst:
-            lst[str(key)] = 1
-        elif str(key) in lst:
+    text = str(text)
+    letter_frequency = {}
+    for i in range(len(text)):
+        key = str(text[i])
+        if not key in letter_frequency:
+            letter_frequency[key] = 1
+        elif key in letter_frequency:
             return False
 
     return True
 
-print(is_isogramm('paradox'))
+print(is_isogramm('pardox'))
 
 
 
