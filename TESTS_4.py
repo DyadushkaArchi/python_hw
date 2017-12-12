456c7c93459e424a52b560b9741c911869763d9d
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-12-12 21:25:55 +0200

    TESTS_4 corrected

--- TESTS_4.py
+++ TESTS_4.py
@@ -7,7 +7,7 @@ if int_user_number.isnumeric() and 0 < int(int(int_user_number) / 10000) < 10:
     int_user_number = int(int_user_number)
     while int_user_number > 0:
         digit = int_user_number % 10
-        int_user_number = int(int_user_number / 10)
+        int_user_number //= 10
 
         if digit % 2 != 0:
                 num *= digit
