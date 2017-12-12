5a42fc5db6bd9a2c053d9cebc551d0b10206eb3a
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-12-12 18:53:35 +0200

    TESTS_4 подправил

--- TESTS_4.py
+++ TESTS_4.py
@@ -4,9 +4,10 @@ num = 1
 int_user_number = input("Введи пятизначное число: ")
 
 if int_user_number.isnumeric() and 0 < int(int(int_user_number) / 10000) < 10:
-    while int(int_user_number) > 0:
-        digit = int(int_user_number) % 10
-        int_user_number = int(int(int_user_number) / 10)
+    int_user_number = int(int_user_number)
+    while int_user_number > 0:
+        digit = int_user_number % 10
+        int_user_number = int(int_user_number / 10)
 
         if digit % 2 != 0:
                 num *= digit
