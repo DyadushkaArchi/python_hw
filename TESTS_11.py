25d70c6601181d6696873a1df19a32c852059b40
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-12-17 12:52:17 +0200

    TESTS_11

--- TESTS_11.py
+++ TESTS_11.py
@@ -2,14 +2,23 @@
 # а нечетные - по убыванию
 
 
-
 def even_sort(matrix):
-       for i in range(len(matrix)):
+       for i in range(len(matrix[0])):
+              current_number1 = matrix[0][i]
+              current_number2 = matrix[1][i]
               if i % 2 == 0:
-                     matrix[i] = matrix.sort
+                  if current_number1 > current_number2:
+                         matrix[0][i] = current_number2
+                         matrix[1][i] = current_number1
               else:
-                     matrix[i] = matrix.sort(key=lambda matrix : matrix.sort(reverse=True))
+                   if current_number1 < current_number2:
+                         matrix[0][i] = current_number2
+                         matrix[1][i] = current_number1
+
        return matrix
 
 
+matrix = [[1, 2, 3, 4, 5, 6],
+          [7, 8, 9, 10, 11, 0]]
 
+print(even_sort(matrix))
