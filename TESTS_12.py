877bdfbc44dd28396bc89e958bb578c1ffd9e119
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-12-19 16:57:49 +0200

    TESTS_12 corrected again:)

--- TESTS_12.py
+++ TESTS_12.py
@@ -5,9 +5,12 @@ user_choice = input('Hello teacher! Click ENTER to start: ')
 exercises = []
 if user_choice == '':
     while len(exercises) < 36:
-        example = '%d * %d' % (random.randint(2, 9), random.randint(2, 9))
-        if not example in exercises:
-            exercises.append(example)
+        rand_number1 = random.randint(2, 9)
+        rand_number2 = random.randint(2, 9)
+        example1 = '%d * %d' % (rand_number1, rand_number2)
+        example2 = '%d * %d' % (rand_number2, rand_number1)
+        if not example1 in exercises and example2 not in exercises:
+            exercises.append(example1)
 
     result = []
 
