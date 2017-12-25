c949f8588f17de044e02b14bce805375a81c954b
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-12-25 11:55:10 +0200

    Python_hw_34 о-моему так лучше:)

--- Python_hw_34.py
+++ Python_hw_34.py
@@ -1,22 +1,19 @@
-class Dot:
+class CircleDot:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, x0, y0, radius):
         self.coordinate_x = x
         self.coordinate_y = y
-
-
-class Circle(Dot):
-    def __init__(self, radius, x0, y0):
-        super().__init__(self, self.coordinate_x, self.coordinate_y)
+        self.center_coord_x = x0
+        self.center_coord_y = y0
         self.radius = radius
-        self.center_coordinate_x = x0
-        self.center_coordinate_y = y0
 
     def is_in(self):
-        equation = ((self.coordinate_x - self.center_coordinate_x)**2 + (self.coordinate_y - self.center_coordinate_y)**2)
-        if equation <= self.radius**2:
-            return True
-        else:
-            return False
+        equation = ((self.coordinate_x - self.center_coord_x) ** 2 + (self.coordinate_y - self.center_coord_y) ** 2)
+        return equation <= (self.radius ** 2)
+
+# (main.py)
+#from Python_hw_34 import CircleDot
+#CircleDot.__init__(CircleDot, 1, 1, 0, 0, 3)
+#print(CircleDot.is_in(CircleDot))
 
 
