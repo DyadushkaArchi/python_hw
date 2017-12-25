23e4dd04cdf022082200b3dd082efdccb11c75aa
DyadushkaArchi <dyadushka_arci@mail.ru>
2017-12-25 11:14:56 +0200

    Python_hw_33 corrected.

--- Python_hw_33.py
+++ Python_hw_33.py
@@ -2,21 +2,19 @@ class Godzilla:
 
     def __init__(self, stomach_volume):
         self.stomach_volume = stomach_volume
+        self.free_stomach_space = stomach_volume
 
     def eat(self, weight):
-        self.current_stomach_volume = self.stomach_volume - weight
-        return self.current_stomach_volume
+        self.free_stomach_space = self.stomach_volume - weight
+        return self.free_stomach_space
 
     def is_full(self):
-        if not self.current_stomach_volume < self.stomach_volume // 10:
-            return False
-        else:
-            return True
-
+        return self.free_stomach_space < (self.stomach_volume // 10)
 
 #(main.py)
 #from Python_hw_33 import Godzilla
 
 #Godzilla.__init__(Godzilla, 100)
+#print(Godzilla.is_full(Godzilla))
 #Godzilla.eat(Godzilla, 93)
 #print(Godzilla.is_full(Godzilla))
